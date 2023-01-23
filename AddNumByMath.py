001f6cc7e528b7b50a0aaa1a658439bb09c8c085
sanas <sanasmath@gmail.com>
2023-01-23 23:44:04 +0530

    complete(Edited)

--- AddNumByMath.py
+++ AddNumByMath.py
@@ -1,12 +1,21 @@
-while True:
-        def k(input):
-         try:
-          n=int(input)
-          print(n/2 * (n+1))
-         except ValueError:
-          print("Idiot,enter a number!!!!")
+print("if you want to close it, please press 'f' ")
+def k(input):
 
-        n=input("Enter the Last Number ")
+         try:
+
+          n=int(input)
+
+          print(n/2 * (n+1))
+          
+         except ValueError:
+          print("Idiot Enter a number!!!!")
+
+while True:
+    n=input("Enter the Last Number ")
+    if n=="f":
+        print("stoped!!!!!")
+        break
+    else:
         k(n)
 
 
